2038ff90eda8cad1687dee0a2169c38640c8027f
Akinn <akinncar@hotmail.com>
2019-11-05 12:58:50 -0300

    Settings iOS

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,13 +1,14 @@
-import { Platform } from 'react-native';
+import {Platform} from 'react-native';
 import produce from 'immer';
 
 const INITIAL_STATE = {
-  signed: Platform !== 'iOS' ? true : false,
+  signed: Platform.OS !== 'ios' ? true : false,
   email: '',
 };
 
 export default function auth(state = INITIAL_STATE, actions) {
   return produce(state, draft => {
+    console.tron.log(Platform);
     switch (actions.type) {
       case '@auth/SIGN_IN_SUCCESS': {
         draft.signed = true;
